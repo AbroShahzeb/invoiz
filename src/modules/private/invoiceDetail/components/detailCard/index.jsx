acbdf6ac3a630f9c6667fc94dd91bf59b7455a80
AbroShahzeb <khizr77@gmail.com>
2025-01-20 20:55:00 +0500

    Changes

--- src/modules/private/invoiceDetail/components/detailCard/index.jsx
+++ src/modules/private/invoiceDetail/components/detailCard/index.jsx
@@ -4,18 +4,18 @@ import { ItemsCard } from "../itemsCard";
 export const DetailCard = ({ invoice }) => {
   console.log("invoice", invoice);
   return (
-    <div className="mt-4 md:mt-6 bg-white rounded-[6px] md:rounded-lg shadow-invoice-card p-6">
+    <div className="mt-4 md:mt-6 bg-white dark:bg-dark-800 rounded-[6px] md:rounded-lg shadow-invoice-card p-6">
       <div>
         <div className="text-heading-sm-variant font-bold hover:underline">
           <span className="text-blue-muted-500">#</span>
           {invoice.id}
         </div>
-        <p className="mt-1 text-body-variant font-medium text-blue-muted-500">
+        <p className="mt-1 text-body-variant font-medium text-blue-muted-500 dark:text-blue-muted-100">
           {invoice.description}
         </p>
       </div>
 
-      <div className="text-body-variant font-medium text-blue-muted-500 mt-[30px]">
+      <div className="text-body-variant font-medium text-blue-muted-500 dark:text-blue-muted-100 mt-[30px]">
         <p>{invoice?.senderAddress?.street}</p>
         <p>{invoice?.senderAddress?.city}</p>
         <p>{invoice?.senderAddress?.postalCode}</p>
@@ -25,7 +25,7 @@ export const DetailCard = ({ invoice }) => {
       <div className="flex items-start gap-10 mt-[31px]">
         <div>
           <div>
-            <p className="text-body-variant font-medium text-blue-muted-500">
+            <p className="text-body-variant font-medium text-blue-muted-500 dark:text-blue-muted-100">
               Invoice Date
             </p>
             <p className="mt-[13px] text-[15px] leading-[20px] font-bold tracking-[-0.25px]">
@@ -41,7 +41,7 @@ export const DetailCard = ({ invoice }) => {
           </div>
 
           <div className="mt-[31px]">
-            <p className="text-body-variant font-medium text-blue-muted-500">
+            <p className="text-body-variant font-medium text-blue-muted-500 dark:text-blue-muted-100">
               Payment Due
             </p>
             <p className="mt-[13px] text-[15px] leading-[20px] font-bold tracking-[-0.25px]">
@@ -58,14 +58,14 @@ export const DetailCard = ({ invoice }) => {
         </div>
 
         <div className="">
-          <p className="text-body-variant font-medium text-blue-muted-500">
+          <p className="text-body-variant font-medium text-blue-muted-500 dark:text-blue-muted-100">
             Bill to
           </p>
           <div className="mt-[13px]">
             <p className="mt-[13px] text-[15px] leading-[20px] font-bold tracking-[-0.25px]">
               {invoice.clientName}
             </p>
-            <div className="text-body-variant font-medium text-blue-muted-500 mt-[30px]">
+            <div className="text-body-variant font-medium text-blue-muted-500 dark:text-blue-muted-100 mt-[30px]">
               <p>{invoice?.clientAddress?.street}</p>
               <p>{invoice?.clientAddress?.city}</p>
               <p>{invoice?.clientAddress?.postalCode}</p>
@@ -76,7 +76,7 @@ export const DetailCard = ({ invoice }) => {
       </div>
 
       <div className="mt-[31px]">
-        <p className="text-body-variant font-medium text-blue-muted-500">
+        <p className="text-body-variant font-medium text-blue-muted-500 dark:text-blue-muted-100">
           Sent to
         </p>
         <p className="mt-[13px] text-[15px] leading-[20px] font-bold tracking-[-0.25px]">
--- src/modules/private/invoiceDetail/components/itemsCard/index.jsx
+++ src/modules/private/invoiceDetail/components/itemsCard/index.jsx
@@ -3,12 +3,12 @@ import { FormattedNumber } from "react-intl";
 export const ItemsCard = ({ invoice }) => {
   return (
     <div className="mt-[38px] rounded-lg overflow-hidden">
-      <div className="p-6 bg-blue-muted-50 flex flex-col gap-6 text-heading-sm-variant font-bold">
+      <div className="p-6 bg-blue-muted-50 dark:bg-dark-700 flex flex-col gap-6 text-heading-sm-variant font-bold">
         {invoice?.items?.map((item, index) => (
           <div key={index} className="flex items-center gap-3 justify-between ">
             <div className="flex flex-col gap-2">
               <p>{item?.name}</p>
-              <p className="text-primary-dim">
+              <p className="text-primary-dim dark:text-blue-muted-300">
                 {item?.quantity} x{" "}
                 <FormattedNumber
                   style="currency"
@@ -31,7 +31,7 @@ export const ItemsCard = ({ invoice }) => {
           </div>
         ))}
       </div>
-      <div className="p-6 flex items-center gap-3 justify-between text-white bg-dark-600">
+      <div className="p-6 flex items-center gap-3 justify-between text-white bg-dark-600 dark:bg-surface-dark">
         <p className="text-[13px] leading-[18px] tracking-[-0.1px]">
           Gross Total
         </p>
